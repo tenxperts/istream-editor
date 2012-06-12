0604613423c74a788a1873799b49849d135cc0e4
Anshuman Rai <anshuman_rai@yahoo.com>
2012-06-12 22:55:45 +0530

    Fix warnings/errors for typecast of GtkWidget/GtkWindow

--- cpp/VideoEditor.cpp
+++ cpp/VideoEditor.cpp
@@ -7,7 +7,7 @@ int main ( int argc , char * argv [])
 	gtk_init (& argc , & argv );
 	xml = glade_xml_new ( "videoeditor.glade" , NULL , NULL );
 	glade_xml_signal_autoconnect(xml);
-	GtkWindow *window = glade_xml_get_widget( xml , "MainWindow" );
+	GtkWidget *window = glade_xml_get_widget( xml , "MainWindow" );
 	gtk_widget_show ( window );
 	gtk_main ();
 	return 0;
